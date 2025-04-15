1732bbe54d4f039878d42dec33115711a35c7e1f
mawaisss <154519230+mawaisss@users.noreply.github.com>
2025-04-15 12:42:07 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const clientsRouter = require('./routes/clients');
+const path = require('path');
 
 app.use(express.json());
 app.use('/api/clients', clientsRouter);
+
+// Serve static files from public folder
+app.use(express.static('public'));
+
+// Root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
